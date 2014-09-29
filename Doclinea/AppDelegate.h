18fe53ecceeb91ee3b4c8d174d6f10ed7545b901
Diego Vidal <diefervidal91@gmail.com>
2014-09-29 15:58:34 -0500

    Primer commit

---
+++ Doclinea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Doclinea
+//
+//  Created by Developer on 29/09/14.
+//  Copyright (c) 2014 iAm Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Doclinea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Doclinea
+//
+//  Created by Developer on 29/09/14.
+//  Copyright (c) 2014 iAm Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
